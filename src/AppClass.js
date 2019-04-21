10b2ce8a5cea1ca4693e631ee257a2f503baf4a0
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-04-21 22:36:17 +0400

    toggle state in class component

--- src/AppClass.js
+++ src/AppClass.js
@@ -2,18 +2,37 @@ import React, { Component } from "react";
 
 class App extends Component {
   state = {
-    count: 0
+    count: 0,
+    isOn: false
   };
 
   incrementCount = () => {
     this.setState(prevState => ({ count: prevState.count + 1 }));
   };
 
+  toggleLight = () => {
+    this.setState(prevState => ({ isOn: !prevState.isOn }));
+  };
+
   render() {
     return (
-      <button onClick={this.incrementCount}>
-        I was clicked {this.state.count} times
-      </button>
+      <>
+        <h2>Counter</h2>
+        <button onClick={this.incrementCount}>
+          I was clicked {this.state.count} times
+        </button>
+
+        <h2>Toggle Light</h2>
+        <div
+          style={{
+            transition: "background 0.3s",
+            height: "50px",
+            width: "50px",
+            background: this.state.isOn ? "yellow" : "grey"
+          }}
+          onClick={this.toggleLight}
+        />
+      </>
     );
   }
 }
